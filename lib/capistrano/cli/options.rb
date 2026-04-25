8b25419152144ccb028a5b86df84892ba7830901
Kuba Suder <jakub.suder@gmail.com>
2026-04-25 03:43:44 +0200

    merged some CLI modules into CLI

--- lib/capistrano/cli/options.rb
+++ lib/capistrano/cli/options.rb
@@ -3,19 +3,6 @@ require 'optparse'
 module Capistrano
   class CLI
     module Options
-      def self.included(base)
-        base.extend(ClassMethods)
-      end
-
-      module ClassMethods
-        # Return a new CLI instance with the given arguments pre-parsed and
-        # ready for execution.
-        def parse(args)
-          cli = new(args)
-          cli.parse_options!
-          cli
-        end
-      end
 
       # The hash of (parsed) command-line options
       attr_reader :options
